9f8cb25a37a2be626e5fa238158eef45df6793ed
Santosh Tarband <santosh.tarband@signifikant.biz>
2020-04-22 16:59:35 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpaces } from "./shared/convert-to-spaces.pipe";
+import { StarComponent } from "./shared/star.component";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpaces,
+    StarComponent,
+  ],
+  imports: [BrowserModule, FormsModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
